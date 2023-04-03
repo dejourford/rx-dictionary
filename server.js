ccbfd08abb6f04677aad61bf6bd3821260d0fae4
dejourford <dejourford@my.unt.edu>
2023-04-02 23:19:00 -0500

    add get request for index page

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = 8
+
+
+// create get request for index page
+app.get('/', (request, response) => {
+    response.sendFile('index.html')
+})
