a2fc9a80b5fa72b1438b9700fb4c509834ac2d53
dejourford <dejourford@my.unt.edu>
2023-04-02 23:28:12 -0500

    create listen function for server

--- server.js
+++ server.js
@@ -5,5 +5,10 @@ const PORT = 8
 
 // create get request for index page
 app.get('/', (request, response) => {
-    response.sendFile('index.html')
+    response.sendFile(__dirname + '/index.html')
+})
+
+// create listen for PORT
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
 })
