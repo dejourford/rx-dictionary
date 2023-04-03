bd363da538bc819fe8d8167318f0284744a664ca
dejourford <dejourford@my.unt.edu>
2023-04-03 11:21:01 -0500

    added drugs to object list

--- server.js
+++ server.js
@@ -4,23 +4,250 @@ const PORT = 8
 
 const drugs = {
     'adderall': {
-        name: 'Adderall',
+        brandName: 'Adderall',
+        genericName: 'D-Amphetamine',
         image: '#',
-        description: 'A small description',
+        description: 'A stimulant used to treat attention deficit disorder (ADD)',
         abusePotential: 'High'
     }, 
-    'bacitracin': {
-        name: 'Bacitracin',
+    'd-amphetamine': {
+        brandName: 'Adderall',
+        genericName: 'D-Amphetamine',
         image: '#',
-        description: 'A small description',
+        description: 'A stimulant used to treat attention deficit disorder (ADD)',
         abusePotential: 'Low'
     },
-    'amoxilcillin': {
-        name: 'Amoxilcillin',
+    'amoxil': {
+        brandName: 'Amoxil',
+        genericName: 'Amoxicillin',
         image: '#',
-        description: 'A small description',
+        description: 'A penicillin antibiotic that fights bacteria',
         abusePotential: 'Low'
-    }
+    },
+    'amoxicillin': {
+        brandName: 'Amoxil',
+        genericName: 'Amoxicillin',
+        image: '#',
+        description: 'A penicillin antibiotic that fights bacteria',
+        abusePotential: 'Low'
+    },
+    'tylenol': {
+        brandName: 'Tylenol',
+        genericName: 'Acetaminophen',
+        image: '#',
+        description: 'A pain reliver and fever reducer',
+        abusePotential: 'Low'
+    },
+    'acetaminophen': {
+        brandName: 'Tylenol',
+        genericName: 'Amoxilcillin',
+        image: '#',
+        description: 'A pain reliever and fever reducer',
+        abusePotential: 'Low'
+    },
+    'elavil': {
+        brandName: 'Elavil',
+        genericName: 'Amitriptyline',
+        image: '#',
+        description: 'An anti-depressant with sedative effects',
+        abusePotential: 'Low'
+    },
+    'amitriptyline': {
+        brandName: 'Elavil',
+        genericName: 'Amitriptyline',
+        image: '#',
+        description: 'An anti-depressant with sedative effects',
+        abusePotential: 'Low'
+    },
+    'norvasc': {
+        brandName: 'Norvasc',
+        genericName: 'Amlodipine',
+        image: '#',
+        description: 'A medicine used to treat high blood pressure (hypertension)',
+        abusePotential: 'Low'
+    },
+    'amlodipine': {
+        brandName: 'Norvasc',
+        genericName: 'Amlodipine',
+        image: '#',
+        description: 'A medicine used to treat high blood pressure (hypertension)',
+        abusePotential: 'Low'
+    },
+    'ativan': {
+        brandName: 'Ativan',
+        genericName: 'Lorazepam',
+        image: '#',
+        description: 'A medicine used to treat anxiety disorders',
+        abusePotential: 'High'
+    },
+    'lorazepam': {
+        brandName: 'Ativan',
+        genericName: 'Lorazepam',
+        image: '#',
+        description: 'A medicine used to treat anxiety disorders',
+        abusePotential: 'High'
+    },
+    'lipitor': {
+        brandName: 'Lipitor',
+        genericName: 'Atorvastatin',
+        image: '#',
+        description: 'A medicine used to treat high cholesterol',
+        abusePotential: 'Low'
+    },
+    'atorvastatin': {
+        brandName: 'Lipitor',
+        genericName: 'Atorvastatin',
+        image: '#',
+        description: 'A medicine used to treat high cholesterol',
+        abusePotential: 'Low'
+    },
+    'zithromax': {
+        brandName: 'Zithromax',
+        genericName: 'Azithromycin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'azithromycin': {
+        brandName: 'Zithromax',
+        genericName: 'Azithromycin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'z-pak': {
+        brandName: 'Zithromax',
+        genericName: 'Azithromycin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'tessalon': {
+        brandName: 'Tessalon',
+        genericName: 'Benzonatate',
+        image: '#',
+        description: 'A medicine used to suppress coughing',
+        abusePotential: 'Low'
+    },
+    'benzonatate': {
+        brandName: 'Tessalon',
+        genericName: 'Benzonatate',
+        image: '#',
+        description: 'A medicine used to suppress coughing',
+        abusePotential: 'Low'
+    },
+    'brilinta': {
+        brandName: 'Brilinta',
+        genericName: 'Ticagrelor',
+        image: '#',
+        description: 'A medicine used to prevent blood clots',
+        abusePotential: 'Low'
+    },
+    'ticagrelor': {
+        brandName: 'Brilinta',
+        genericName: 'Ticagrelor',
+        image: '#',
+        description: 'A medicine used to prevent blood clots',
+        abusePotential: 'Low'
+    },
+    'subutex': {
+        brandName: 'Subutex',
+        genericName: 'Buprenorphine',
+        image: '#',
+        description: 'A medicine used to treat opioid addiciton',
+        abusePotential: 'High'
+    },
+    'buprenorphine': {
+        brandName: 'Subutex',
+        genericName: 'Buprenorphine',
+        image: '#',
+        description: 'A medicine used to treat opiod addiction',
+        abusePotential: 'High'
+    },
+    'keflex': {
+        brandName: 'Keflex',
+        genericName: 'Cephalexin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'cephalexin': {
+        brandName: 'Keflex',
+        genericName: 'Cephalexin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'cipro': {
+        brandName: 'Cipro',
+        genericName: 'Ciprofloxacin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'ciprofloxacin': {
+        brandName: 'Cipro',
+        genericName: 'Ciprofloxacin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'celexa': {
+        brandName: 'Celexa',
+        genericName: 'Citalopram',
+        image: '#',
+        description: 'A medicine used to treat depression or mood',
+        abusePotential: 'Low'
+    },
+    'citalopram': {
+        brandName: 'Celexa',
+        genericName: 'Citalopram',
+        image: '#',
+        description: 'A medicine used to treat depression or mood',
+        abusePotential: 'Low'
+    },
+    'cleocin': {
+        brandName: 'Cleocin',
+        genericName: 'Clindamycin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'clindamycin': {
+        brandName: 'Cleocin',
+        genericName: 'Clindamycin',
+        image: '#',
+        description: 'An antibiotic used to fight bacteria',
+        abusePotential: 'Low'
+    },
+    'klonopin': {
+        brandName: 'Klonopin',
+        genericName: 'Clonazepam',
+        image: '#',
+        description: 'A medicine used to treat seizure and panic disorders',
+        abusePotential: 'High'
+    },
+    'clonazepam': {
+        brandName: 'Klonopin',
+        genericName: 'Clonazepam',
+        image: '#',
+        description: 'A medicine used to treat seizure and panic disorders',
+        abusePotential: 'High'
+    },
+    'flexeril': {
+        brandName: 'Flexeril',
+        genericName: 'Cyclobenzaprine',
+        image: '#',
+        description: 'A medicine used to relax muscles',
+        abusePotential: 'Medium'
+    },
+    'cyclobenzaprine': {
+        brandName: 'Flexeril',
+        genericName: 'Cyclobenzaprine',
+        image: '#',
+        description: 'A medicine used to relax muscles',
+        abusePotential: 'Medium'
+    },
 }
 
 
