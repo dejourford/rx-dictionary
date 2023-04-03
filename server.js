2b25d2fe4f19a0c2e417fbde21952d39b44c2531
dejourford <dejourford@my.unt.edu>
2023-04-03 05:33:46 -0500

    add get request for api and env PORT

--- server.js
+++ server.js
@@ -2,13 +2,53 @@ const express = require('express')
 const app = express()
 const PORT = 8
 
+const drugs = {
+    'adderall': {
+        name: 'Adderall',
+        image: '#',
+        description: 'A small description',
+        abusePotential: 'High'
+    }, 
+    'bacitracin': {
+        name: 'Bacitracin',
+        image: '#',
+        description: 'A small description',
+        abusePotential: 'Low'
+    },
+    'amoxilcillin': {
+        name: 'Amoxilcillin',
+        image: '#',
+        description: 'A small description',
+        abusePotential: 'Low'
+    }
+}
+
+
+
 
 // create get request for index page
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
+// create get request for style sheet
+app.get('/styles.css', (request, response) => {
+    response.sendFile(__dirname + '/styles.css')
+})
+
+// create get request for js file
+app.get('/main.js', (request, response) => {
+    response.sendFile(__dirname + '/main.js')
+})
+
+
+// create get request for API
+app.get('/api/', (request, response) => {
+    response.json(drugs)
+})
+
+
 // create listen for PORT
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is running on port ${PORT}`)
 })
